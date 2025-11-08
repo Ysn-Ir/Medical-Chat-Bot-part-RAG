5213bef4453e0066fd9c00be500d9e14d9460e8d
yassine <oualiy419@gmail.com>
2025-11-08 19:24:13 +0100

    project structure

---
+++ terminal.sh
@@ -0,0 +1,14 @@
+mkdir -p src
+mkdir -p research 
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+
+touch .env
+touch setup.py 
+touch app.py
+touch research/trial.py
+touch requirements.txt
+
+echo "All files and directories have been created succesfully"
