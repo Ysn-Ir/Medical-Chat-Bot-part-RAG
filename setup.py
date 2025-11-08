50da7ab7967cb40b7a47ade0bbbd4c479fe1da3d
yassine <oualiy419@gmail.com>
2025-11-08 19:51:13 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name="medical chat bot",
+    version="0.0.1",
+    author="Yassine Ouali",
+    author_email="Oualiy419@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
